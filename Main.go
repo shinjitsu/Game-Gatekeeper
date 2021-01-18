5ec49aef2097478eb477b50e195783bbd80d2863
TheDoomForce <malkite1@auxilioabalto.me>
2021-01-18 12:49:35 -0500

    now runs continuously and takes screenshots while killing processes and verbally scolding.
    attempt at a window as well has been removed since it was problematic.

--- Main.go
+++ Main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/hegedustibor/htgo-tts"
+	"github.com/kbinani/screenshot"
 	ps "github.com/mitchellh/go-ps"
+	"image/png"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -14,26 +17,69 @@ func main() {
 }
 
 func runLoop() {
-	//	for{
-	currentProccesses := getProcesses()
-	//		numProcesses := len(currentProccesses)
-	for _, info := range currentProccesses {
-		for _, gameName := range gameInfo {
-			if strings.Contains(info.Executable(), gameName) {
-				fmt.Println(info.Executable())
-				proc, err := os.FindProcess(info.Pid())
-				if err != nil {
-					log.Println("can't kill ", info.Executable())
+
+	for {
+		currentProccesses := getProcesses()
+		//		numProcesses := len(currentProccesses)
+		for _, info := range currentProccesses {
+			for _, gameName := range gameInfo {
+				if strings.Contains(info.Executable(), gameName) {
+					takeScreenshot()
+					time.Sleep(5 * time.Millisecond)
+					proc, err := os.FindProcess(info.Pid())
+					if err != nil {
+						log.Println("can't kill ", info.Executable())
+					}
+					proc.Kill()
+					speech := htgotts.Speech{Folder: "audio", Language: "en"}
+					speech.Speak("Stop playing Games!")
+
+					//				scoldWindow := makeScoldWindow()
+					//				scoldWindow.ShowAndRun()
 				}
-				proc.Kill()
-				speech := htgotts.Speech{Folder: "audio", Language: "en"}
-				speech.Speak("Stop playing Games!")
 			}
 		}
+		time.Sleep(5 * time.Second)
 	}
-	//	time.Sleep(5000)
-	//	}
 }
+
+func takeScreenshot() {
+	//pulled directly from the readme https://github.com/kbinani/screenshot
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", time.Now(), bounds.Dx(), bounds.Dy())
+		file, _ := os.Create(fileName)
+		defer file.Close()
+		png.Encode(file, img)
+
+		fmt.Printf("#%d : %v \"%s\"\n", time.Now(), bounds, fileName)
+	}
+}
+
+//func makeScoldWindow() fyne.Window{
+//	fyneApp := app.New()
+//	fyneWindow := fyneApp.NewWindow("Games Not Yet Authorized!!!!")
+//	layout := container.NewVBox()
+//	style:= fyne.TextStyle{
+//		Bold: true,
+//	}
+//	scoldText := widget.NewLabelWithStyle("GAMES ARE NOT AUTHORIZED YET!", fyne.TextAlignCenter, style)
+//	layout.Add(scoldText)
+//	oopsButton := widget.NewButton("Oppps I shoulda known that", func() {
+//			fyneWindow.Close()
+//	})
+//	layout.Add(oopsButton)
+//	fyneWindow.SetContent(layout)
+//
+//	return fyneWindow
+//}
 
 func getProcesses() []ps.Process {
 	allProceeses, err := ps.Processes()
--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module Game-Gatekeeper
 go 1.15
 
 require (
+	fyne.io/fyne v1.4.3
+	github.com/BurntSushi/xgb v0.0.0-20201008132610-5f9e7b3c49cd // indirect
+	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
 	github.com/hegedustibor/htgo-tts v0.0.0-20201020054059-2b0b3a7cc05a
+	github.com/kbinani/screenshot v0.0.0-20191211154542-3a185f1ce18f
+	github.com/lxn/win v0.0.0-20201111105847-2a20daff6a55 // indirect
 	github.com/mitchellh/go-ps v1.0.0
 )
